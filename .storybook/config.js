331956848b12a6127d04b6c5a1a263a4b2ce1ea0
João Sardinha <johnsardine@gmail.com>
2018-06-28 11:35:05 +0100

    Configure storybook ui

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,14 @@
 import { configure } from '@storybook/vue';
+import { setOptions } from '@storybook/addon-options';
 import Vue from 'vue';
 
 function loadStories() {
 	require('../stories');
 }
+
+setOptions({
+	name: 'Samora UI',
+	url: 'https://unbabel.gitlab.io/ui/',
+});
 
 configure(loadStories, module);
