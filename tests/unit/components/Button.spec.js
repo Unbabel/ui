1d37fc9e5df395ab3492b1750a7adce3877f3f86
Sofia F. Rocha <sofiaroc.pt@gmail.com>
2018-07-10 17:29:36 +0100

    Adds tests for the Button component

--- tests/unit/components/Button.spec.js
+++ tests/unit/components/Button.spec.js
@@ -1,11 +1,43 @@
-import {
-	mount,
-} from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
 import Button from '../../../src/components/Button.vue';
 
 describe('Button', () => {
-	it('mount properly', () => {
+	it('mounts properly', () => {
 		const wrapper = mount(Button);
 		expect(wrapper.html()).toBe('<button class="c-Button c-Button--secondary">Click me</button>');
+	});
+
+	it('adds the right classes', () => {
+		const wrapper = mount(Button, {
+			propsData: {
+				kind: 'cta',
+			},
+		});
+		expect(wrapper.html()).toBe('<button class="c-Button c-Button--cta">Click me</button>');
+	});
+
+	it('calls the clickHandler when clicked', () => {
+		const wrapper = mount(Button, {
+			propsData: {
+				clickHandler: jest.fn(),
+			},
+		});
+		wrapper.trigger('click');
+
+		expect(wrapper.vm.clickHandler).toHaveBeenCalled();
+	});
+
+	it('opens the link if it exists', () => {
+		window.open = jest.fn();
+		const wrapper = mount(Button, {
+			propsData: {
+				clickHandler: jest.fn(),
+				link: 'http://google.com',
+			},
+		});
+		wrapper.trigger('click');
+
+		expect(wrapper.vm.clickHandler).toHaveBeenCalled();
+		expect(window.open).toHaveBeenCalled();
 	});
 });
