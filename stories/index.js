706cb6f269c79fd10c6d0fd248882abe0d341ec8
João Sardinha <johnsardine@gmail.com>
2018-08-27 15:35:20 +0100

    Show only modal component in the props table

--- stories/index.js
+++ stories/index.js
@@ -112,6 +112,9 @@ storiesOf('Modal', module)
 		summary: `The simplest Modal shows a message and a button for user acknowledgement.<br>
 			It also supports longer texts that trigger scrollbars, having a close button on the corner and having a form inside. You can put whatever elements you want on the bottom. By default it has the least destructive option selected (in this examples' case, the Cancel button).
 		`,
+		propTables: [
+			Modal,
+		],
 	})(() => {
 		return {
 			data: () => {
