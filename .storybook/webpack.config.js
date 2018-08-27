5ed9cdd7ddca7abe7f5c47d29b3a2b03d77f454b
João Sardinha <johnsardine@gmail.com>
2018-08-27 18:38:28 +0100

    Update storybook webpack indentation

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,4 +1,4 @@
-const path = require("path");
+const path = require('path');
 
 // Export a function. Accept the base config as the only param.
 module.exports = (storybookBaseConfig, configType) => {
@@ -6,7 +6,7 @@ module.exports = (storybookBaseConfig, configType) => {
   // You can change the configuration based on that.
   // 'PRODUCTION' is used when building the static version of storybook.
 
-	storybookBaseConfig.resolve.alias['@'] = path.resolve(__dirname, '../src/');
+  storybookBaseConfig.resolve.alias['@'] = path.resolve(__dirname, '../src/');
 
   // Return the altered config
   return storybookBaseConfig;
