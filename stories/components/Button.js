3f7f5ea6917fdab8ef3456620d8ddc959af6aa70
João Sardinha <johnsardine@gmail.com>
2018-08-28 15:37:03 +0100

    Improve button documentation

--- stories/components/Button.js
+++ stories/components/Button.js
@@ -4,25 +4,23 @@ import { withKnobs, text, boolean, select } from '@storybook/addon-knobs/vue';
 import { withInfo } from 'storybook-addon-vue-info';
 
 import Button from '@/components/Button.vue';
+import ButtonSummary from './Button.md';
 
 storiesOf('Button', module)
 	.add('Usage', withInfo({
-		summary: `This is basic button: you click it and stuff happens. Hopefully.<br>
-			Besides its default state, it has 3 visual variations. It can also be disabled.`,
+		summary: ButtonSummary,
 	})(() => {
-		const content = text('Button text', 'Click me');
-		const link = text('Button link', '');
-		const kind = select('Kind', {
-			primary: 'Primary',
-			secondary: 'secondary',
-			cta: 'Call to Action',
-			ctaAlt: 'Call to Action (Alternative)',
-		}, 'primary');
-		const isDisabled = boolean('Disabled', false);
-
 		return {
-			components: { btn: Button },
-			template: `<btn :kind="'${kind}'" :click-handler="log" :disabled="${isDisabled}" :link="'${link}'">${content}</btn>`,
+			components: { Button },
+			methods: {
+				handleClick: action('Button clicked'),
+			},
+			template: '<Button kind="primary" @click="handleClick">Click me</Button>',
+			propsDescription: {
+				kind: 'One of [primary, secondary, cta, ctaAlt]',
+				href: 'When used, Button behaves just like a <a> element',
+				disabled: 'Disables the Button element. When used together with \'href\' it will have the class \'is-disabled\'',
+			},
 		};
 	}))
 	.addDecorator(withKnobs)
@@ -39,7 +37,7 @@ storiesOf('Button', module)
 
 		return {
 			components: { btn: Button },
-			template: `<btn :kind="'${kind}'" @click="log" :disabled="${isDisabled}" :href="'${link}'">${content}</btn>`,
+			template: `<btn kind="${kind}" @click="log" :disabled="${isDisabled}" href="${link}">${content}</btn>`,
 			methods: {
 				log: action('clicked the default button'),
 			},
@@ -51,7 +49,7 @@ storiesOf('Button', module)
 
 		return {
 			components: { btn: Button },
-			template: `<btn :kind="'primary'" @click="log" :disabled="${isDisabled}">${content}</btn>`,
+			template: `<btn kind="primary" @click="log" :disabled="${isDisabled}">${content}</btn>`,
 			methods: {
 				log: action('clicked the primary button'),
 			},
@@ -63,7 +61,7 @@ storiesOf('Button', module)
 
 		return {
 			components: { btn: Button },
-			template: `<btn :kind="'secondary'" @click="log" :disabled="${isDisabled}">${content}</btn>`,
+			template: `<btn kind="secondary" @click="log" :disabled="${isDisabled}">${content}</btn>`,
 			methods: {
 				log: action('clicked the secondary button'),
 			},
@@ -75,7 +73,7 @@ storiesOf('Button', module)
 
 		return {
 			components: { btn: Button },
-			template: `<btn :kind="'cta'" @click="log" :disabled="${isDisabled}">${content}</btn>`,
+			template: `<btn kind="cta" @click="log" :disabled="${isDisabled}">${content}</btn>`,
 			methods: {
 				log: action('clicked the CTA button'),
 			},
@@ -87,7 +85,7 @@ storiesOf('Button', module)
 
 		return {
 			components: { btn: Button },
-			template: `<btn :kind="'ctaAlt'" @click="log" :disabled="${isDisabled}">${content}</btn>`,
+			template: `<btn kind="ctaAlt" @click="log" :disabled="${isDisabled}">${content}</btn>`,
 			methods: {
 				log: action('clicked the CTA button'),
 			},
---
+++ stories/components/Button.md
@@ -0,0 +1,3 @@
+This is basic button: you click it and stuff happens. Hopefully.
+
+Besides its default state, it has 3 visual variations. It can also be disabled.
