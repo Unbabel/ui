d3dffab174793c6d1ce5a864229d4519a26f86ca
Sofia F. Rocha <sofiaroc.pt@gmail.com>
2018-09-26 11:12:41 +0100

    Update the tests for the Button component

--- tests/unit/components/Button.spec.js
+++ tests/unit/components/Button.spec.js
@@ -12,6 +12,17 @@ describe('Button', () => {
 		});
 		expect(wrapper.html()).toEqual('<button class="c-Button">Click me!</button>');
 	});
+	it("mounts properly if it's a <a> element", () => {
+		const wrapper = mount(Button, {
+			slots: {
+				default: 'Click me!',
+			},
+			propsData: {
+				href: 'www.google.com',
+			},
+		});
+		expect(wrapper.html()).toEqual('<a href="www.google.com" class="c-Button">Click me!</a>');
+	});
 	it('passes a valid kind prop', () => {
 		const wrapper = mount(Button, {
 			slots: {
@@ -34,39 +45,5 @@ describe('Button', () => {
 			},
 		});
 		expect(wrapper.html()).toEqual('<button class="c-Button c-Button--primary c-Button--big">Olá!</button>');
-	});
-
-	it('adds the right classes', () => {
-		const wrapper = mount(Button, {
-			propsData: {
-				kind: 'cta',
-			},
-		});
-		expect(wrapper.html()).toBe('<button class="c-Button c-Button--cta">Click me</button>');
-	});
-
-	it('calls the clickHandler when clicked', () => {
-		const wrapper = mount(Button, {
-			propsData: {
-				clickHandler: jest.fn(),
-			},
-		});
-		wrapper.trigger('click');
-
-		expect(wrapper.vm.clickHandler).toHaveBeenCalled();
-	});
-
-	it('opens the link if it exists', () => {
-		window.open = jest.fn();
-		const wrapper = mount(Button, {
-			propsData: {
-				clickHandler: jest.fn(),
-				link: 'http://google.com',
-			},
-		});
-		wrapper.trigger('click');
-
-		expect(wrapper.vm.clickHandler).toHaveBeenCalled();
-		expect(window.open).toHaveBeenCalled();
 	});
 });
