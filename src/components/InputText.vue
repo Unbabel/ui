06561bb6972c5934c74b5b2e38a5be31b348b112
João Sardinha <johnsardine@gmail.com>
2018-11-12 20:24:11 +0000

    Convert InputText and Select indentation to tabs

--- src/components/InputText.vue
+++ src/components/InputText.vue
@@ -1,18 +1,18 @@
 <template>
-  <label class="c-FormInputText" :class="classObject" :disabled="disabled">
-    <div class="c-FormInputText__label" v-show="showLabel">{{ label }}</div>
-    <div class="c-FormInputText__controlContainer">
-      <input
-        :type="type"
-        :name="name"
-        :value="value"
-        :required="required"
-        :disabled="disabled"
-        :placeholder="placeholder"
-        class="c-FormInputText__control"
-        @input="onInput">
-    </div>
-  </label>
+	<label class="c-FormInputText" :class="classObject" :disabled="disabled">
+		<div class="c-FormInputText__label" v-show="showLabel">{{ label }}</div>
+		<div class="c-FormInputText__controlContainer">
+			<input
+				:type="type"
+				:name="name"
+				:value="value"
+				:required="required"
+				:disabled="disabled"
+				:placeholder="placeholder"
+				class="c-FormInputText__control"
+				@input="onInput">
+		</div>
+	</label>
 </template>
 
 <script>
@@ -23,73 +23,73 @@ export default {
 	mixins: [
 		SizeMixin,
 	],
-  model: {
-    prop: 'value',
-    event: 'changeModel',
-  },
-  props: {
-    label: {
-      type: String,
-      required: true,
-      default: () => '',
-    },
-    placeholder: {
-      type: String,
-      required: false,
-      default: () => undefined,
-    },
-    type: {
-      type: String,
-      required: false,
-      default: () => 'text',
-    },
-    name: {
-      type: String,
-      required: false,
-    },
-    value: {
-      type: [String],
-      required: false,
-    },
-    required: {
-      type: Boolean,
-      required: false,
-      default: () => false,
-    },
-    disabled: {
-      type: Boolean,
-      required: false,
-      default: () => false,
-    },
-    showLabel: {
-      type: Boolean,
-      required: false,
-      default: () => true,
-    },
+	model: {
+		prop: 'value',
+		event: 'changeModel',
+	},
+	props: {
+		label: {
+			type: String,
+			required: true,
+			default: () => '',
+		},
+		placeholder: {
+			type: String,
+			required: false,
+			default: () => undefined,
+		},
+		type: {
+			type: String,
+			required: false,
+			default: () => 'text',
+		},
+		name: {
+			type: String,
+			required: false,
+		},
+		value: {
+			type: [String],
+			required: false,
+		},
+		required: {
+			type: Boolean,
+			required: false,
+			default: () => false,
+		},
+		disabled: {
+			type: Boolean,
+			required: false,
+			default: () => false,
+		},
+		showLabel: {
+			type: Boolean,
+			required: false,
+			default: () => true,
+		},
 	},
 	computed: {
-    classObject() {
-      return {
-        [`c-FormInputText--${this.size}`]: this.size.length,
-      };
-    },
-  },
-  watch: {
-    selected(newValue) {
-      if (newValue === undefined) {
-        const {
-          select,
-        } = this.$refs;
-        select.selectedIndex = undefined;
-      }
-    },
-  },
-  methods: {
-    onInput(event) {
-      this.$emit('input', event);
-      this.$emit('changeModel', event.target.value);
-    },
-  },
+		classObject() {
+			return {
+				[`c-FormInputText--${this.size}`]: this.size.length,
+			};
+		},
+	},
+	watch: {
+		selected(newValue) {
+			if (newValue === undefined) {
+				const {
+					select,
+				} = this.$refs;
+				select.selectedIndex = undefined;
+			}
+		},
+	},
+	methods: {
+		onInput(event) {
+			this.$emit('input', event);
+			this.$emit('changeModel', event.target.value);
+		},
+	},
 };
 </script>
 
--- src/components/Select.vue
+++ src/components/Select.vue
@@ -1,19 +1,19 @@
 <template>
-  <label class="c-FormSelect" :class="classObject" :disabled="disabled">
-    <div class="c-FormSelect__label" v-show="showLabel">{{ label }}</div>
-    <div class="c-FormSelect__controlContainer">
-      <select
-        class="c-FormSelect__control"
-        ref="select"
-        :name="name"
-        @change="onChange"
-        :disabled="disabled">
-        <option v-if="placeholder" selected disabled>{{ placeholder }}</option>
-        <slot />
-      </select>
-      <span class="c-FormSelect__decorator"></span>
-    </div>
-  </label>
+	<label class="c-FormSelect" :class="classObject" :disabled="disabled">
+		<div class="c-FormSelect__label" v-show="showLabel">{{ label }}</div>
+		<div class="c-FormSelect__controlContainer">
+			<select
+				class="c-FormSelect__control"
+				ref="select"
+				:name="name"
+				@change="onChange"
+				:disabled="disabled">
+				<option v-if="placeholder" selected disabled>{{ placeholder }}</option>
+				<slot />
+			</select>
+			<span class="c-FormSelect__decorator"></span>
+		</div>
+	</label>
 </template>
 
 <script>
@@ -27,53 +27,53 @@ export default {
 	mixins: [
 		SizeMixin,
 	],
-  model: {
-    prop: 'selected',
-    event: 'change',
-  },
-  props: {
-    label: {
-      type: String,
-      required: true,
-      default: () => '',
-    },
-    name: {
-      type: String,
-      required: false,
-    },
-    placeholder: {
-      type: String,
-      required: false,
-      default: () => undefined,
-    },
-    selected: {
-      type: [Number, String, Object, Array],
-      required: false,
-    },
-    disabled: {
-      type: Boolean,
-      required: false,
-      default: () => false,
-    },
-    showLabel: {
-      type: Boolean,
-      required: false,
-      default: () => true,
-    },
-  },
-  computed: {
-    classObject() {
-      return {
-        [`c-FormSelect--${this.size}`]: this.size.length,
-      };
-    },
-  },
-  watch: {
+	model: {
+		prop: 'selected',
+		event: 'change',
+	},
+	props: {
+		label: {
+			type: String,
+			required: true,
+			default: () => '',
+		},
+		name: {
+			type: String,
+			required: false,
+		},
+		placeholder: {
+			type: String,
+			required: false,
+			default: () => undefined,
+		},
+		selected: {
+			type: [Number, String, Object, Array],
+			required: false,
+		},
+		disabled: {
+			type: Boolean,
+			required: false,
+			default: () => false,
+		},
+		showLabel: {
+			type: Boolean,
+			required: false,
+			default: () => true,
+		},
+	},
+	computed: {
+		classObject() {
+			return {
+				[`c-FormSelect--${this.size}`]: this.size.length,
+			};
+		},
+	},
+	watch: {
 		selected(newValue, oldValue) {
 			this.selectNativeOption(newValue);
 		},
-  },
-  methods: {
+	},
+	methods: {
 		selectNativeOption(newValue) {
 			const {
 				select,
@@ -101,8 +101,8 @@ export default {
 				}
 			});
 		},
-    onChange(event) {
-      const {
+		onChange(event) {
+			const {
 				target: select,
 			} = event;
 			const selectedOption = select.options[select.options.selectedIndex];
@@ -110,9 +110,9 @@ export default {
 			// _value is accessing the original vue value, allows emiting actual objects
 			const selectedOptionValue = (selectedOption.hasOwnProperty('_value')) ? selectedOption._value : selectedOption.value;
 
-      // eslint-disable-next-line
-      this.$emit('change', selectedOptionValue);
-    },
+			// eslint-disable-next-line
+			this.$emit('change', selectedOptionValue);
+		},
 	},
 	mounted() {
 		this.selectNativeOption(this.selected);
